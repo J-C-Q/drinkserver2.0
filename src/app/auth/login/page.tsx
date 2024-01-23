6b4850eb6648f34e19c869a35aac46da1a7d8926
Quinten Preiß <preissquinten@gmail.com>
2024-01-23 01:55:52 +0100

    auth first hour

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+import { LoginForm } from "@/components/auth/login-form";
+
+const LogonPage = () => {
+  return <LoginForm />;
+};
+
+export default LogonPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,35 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export default function Home() {
   return (
-    <>
-      <header className="text-4xl flex justify-center items-center">
-        Drink Server
-      </header>
-      <main className="text-xl flex justify-center items-center">
-        Drink Server 2.0
-      </main>
-      <footer className="text-sm flex justify-center items-center">
-        Drink Server 2021
-      </footer>
-    </>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication sevice</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
