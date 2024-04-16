b5925165a081417c2de4e540dee735a06d658cb5
Quinten Preiss <qpreiss@smail.uni-koeln.de>
2024-04-16 18:36:43 +0200

    early bird fix

--- src/components/stats/mainstats.tsx
+++ src/components/stats/mainstats.tsx
@@ -12,25 +12,25 @@ export const MainStats = (
 ) => {
   return (
     <div className="grid grid-rows-1 grid-cols-2 gap-5 pt-0 w-[100%] m-auto relative ">
-      <div className="bg-gradient-to-tr from-[#FF6B6B] to-[#dd4d51] grow text-center aspect-square text-5xl flex justify-center items-center relative bg-clip-text text-transparent translate-y-[-10%] m-auto">
+      <div className="bg-gradient-to-tr from-[#FF6B6B] to-[#dd4d51] grow text-center aspect-square text-5xl flex justify-center items-center relative bg-clip-text text-transparent translate-y-[-10%] m-auto font-bold">
         {totalSugar ? (totalSugar / 1000).toFixed(2) : 0}
-        <div className="absolute text-sm left-[50%] translate-x-[-150%] top-[50%] translate-y-[-200%] text-gray-600 not-italic">
+        <div className="absolute text-sm left-[50%] translate-x-[-140%] top-[50%] translate-y-[-200%] text-gray-600 not-italic">
           sugar
         </div>
-        <div className="absolute text-sm  text-gray-600 not-italic left-[50%] translate-x-[350%] top-[50%] translate-y-[10%]">
+        <div className="absolute text-sm  text-gray-600 not-italic left-[50%] translate-x-[300%] top-[50%] translate-y-[10%]">
           kg
         </div>
       </div>
-      <div className="bg-gradient-to-tr from-[#FFD700] to-[#ddb900] grow text-center aspect-square text-5xl flex justify-center items-center relative bg-clip-text text-transparent translate-y-[10%] m-auto">
+      <div className="bg-gradient-to-tr from-[#FFD700] to-[#ddb900] grow text-center aspect-square text-5xl flex justify-center items-center relative bg-clip-text text-transparent translate-y-[10%] m-auto font-bold">
         {totalCaffein ? (totalCaffein / 1000).toFixed(2) : 0}
-        <div className="absolute text-sm left-[50%] translate-x-[-150%] top-[50%] translate-y-[-200%] text-gray-600 not-italic">
+        <div className="absolute text-sm left-[50%] translate-x-[-100%] top-[50%] translate-y-[-200%] text-gray-600 not-italic">
           caffeine
         </div>
-        <div className="absolute text-sm  text-gray-600 not-italic left-[50%] translate-x-[600%] top-[50%] translate-y-[10%]">
+        <div className="absolute text-sm  text-gray-600 not-italic left-[50%] translate-x-[550%] top-[50%] translate-y-[10%]">
           g
         </div>
       </div>
-      <span className="w-full text-sm text-center absolute bottom-[30px] translate-y-[100%] text-gray-600">
+      <span className="w-full text-sm text-center absolute bottom-[20px] translate-y-[100%] text-gray-600">
         Your stats
       </span>
     </div>
