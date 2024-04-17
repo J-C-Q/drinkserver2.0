34fb6a13d574e5b0eea710880febdf24c958340a
Quinten Preiss <qpreiss@smail.uni-koeln.de>
2024-04-17 17:41:59 +0200

    stats

--- src/components/stats/mainstats.tsx
+++ src/components/stats/mainstats.tsx
@@ -41,41 +41,38 @@ export const MainStats = (
     lastWeekCaffein,
     lastMonthCaffein,
   ];
-  const times = ["total", "day", "week", "month"];
-  let [sugarPointer, setSugarPointer] = useState(0);
-  let [caffeinePointer, setCaffeinePointer] = useState(0);
+  const times = ["total", "today", "week", "month"];
+  let [pointer, setPointer] = useState(0);
   return (
-    <div className="grid grid-rows-1 grid-cols-2 gap-5 pt-0 w-[100%] m-auto relative h-28">
-      <div
-        className="bg-gradient-to-tr from-[#FF6B6B] to-[#dd4d51] grow text-center aspect-square text-5xl flex justify-center items-center relative bg-clip-text text-transparent translate-y-[-10px] m-auto font-bold w-36"
-        onClick={() => {
-          setSugarPointer((sugarPointer + 1) % 4);
-        }}
-      >
-        {((sugarStats[sugarPointer ?? 0] ?? 0) / 1000 ?? 0).toFixed(2)}
+    <div
+      className="grid grid-rows-1 grid-cols-2 gap-5 pt-0 w-[100%] m-auto relative h-28"
+      onClick={() => {
+        setPointer((pointer + 1) % 4);
+      }}
+    >
+      <div className="bg-gradient-to-tr from-[#FF6B6B] to-[#dd4d51] grow text-center aspect-square text-5xl flex justify-center items-center relative bg-clip-text text-transparent translate-y-[-10px] m-auto font-bold w-36">
+        {((sugarStats[pointer ?? 0] ?? 0) / 1000 ?? 0).toFixed(2)}
         <div className="absolute text-sm left-[50%] translate-x-[-60px] top-[50%] translate-y-[-40px] text-gray-600 not-italic whitespace-nowrap">
-          sugar / {times[sugarPointer]}
+          sugar
         </div>
-        <div className="absolute text-sm  text-gray-600 not-italic left-[50%] translate-x-[320%] top-[50%] translate-y-[10%]">
+        <div className="absolute text-sm  text-gray-600 not-italic left-[50%] translate-x-[50px] top-[50%] translate-y-[10%]">
           kg
         </div>
       </div>
-      <div
-        className="bg-gradient-to-tr from-[#FFD700] to-[#ddb900] grow text-center aspect-square text-5xl flex justify-center items-center relative bg-clip-text text-transparent translate-y-[10px] m-auto font-bold w-36"
-        onClick={() => {
-          setCaffeinePointer((caffeinePointer + 1) % 4);
-        }}
-      >
-        {((caffeineStats[caffeinePointer ?? 0] ?? 0) / 1000).toFixed(2)}
+      <div className="bg-gradient-to-tr from-[#FFD700] to-[#ddb900] grow text-center aspect-square text-5xl flex justify-center items-center relative bg-clip-text text-transparent translate-y-[10px] m-auto font-bold w-36">
+        {((caffeineStats[pointer ?? 0] ?? 0) / 1000).toFixed(2)}
         <div className="absolute text-sm left-[50%] translate-x-[-60px] top-[50%] translate-y-[-40px] text-gray-600 not-italic whitespace-nowrap">
-          caffeine / {times[caffeinePointer]}
+          caffeine
         </div>
-        <div className="absolute text-sm  text-gray-600 not-italic left-[50%] translate-x-[580%] top-[50%] translate-y-[10%]">
+        <div className="absolute text-sm  text-gray-600 not-italic left-[50%] translate-x-[50px] top-[50%] translate-y-[10%]">
           g
         </div>
       </div>
       <span className="w-full text-sm text-center absolute bottom-[0px] translate-y-[100%] text-gray-600">
         Your stats
+      </span>
+      <span className="w-full text-lg font-bold text-center absolute top-[-25px] translate-y-[100%] text-gray-600">
+        {times[pointer]}
       </span>
     </div>
   );
